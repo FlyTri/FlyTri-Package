8ee9e063d1a9b04b62a9dccb3f22e5b89799d597
Võ Minh Trí <vominhtri28032010@gmail.com>
2022-04-30 17:47:05 +0700

    Updates

--- src/checkUpdate.js
+++ src/checkUpdate.js
@@ -4,15 +4,15 @@ async function checkUpdate() {
     } catch (e) {
         return;
     }
-    const packageData = await require('axios')(`https://registry.npmjs.com/flytri`).then(text => text.json())
-    if (require('../package.json').version !== packageData['dist-tags'].latest) {
+    const packageData = await require('axios')(`https://registry.npmjs.com/flytri`)
+    if (require('../package.json').version !== packageData.data['dist-tags'].latest) {
         console.log('\n\n')
         console.log('\x1b[32m' + '---------------------------------------------------')
         console.log('\x1b[32m' + '| @ flytri                                 - [] X |')
         console.log('\x1b[32m' + '---------------------------------------------------')
         console.log('\x1b[33m' + `|            The module is\x1b[31m out of date!\x1b[33m           |`)
         console.log('\x1b[35m' + '|             New version is available!           |')
-        console.log('\x1b[34m' + `|                  ${require('../package.json').version} --> ${packageData['dist-tags'].latest}                |`)
+        console.log('\x1b[34m' + `|                  ${require('../package.json').version} --> ${packageData.data['dist-tags'].latest}                |`)
         console.log('\x1b[36m' + '|             Run "npm i flytri@latest"           |')
         console.log('\x1b[36m' + '|                    to update!                   |')
         console.log('\x1b[37m' + `|          View the full changelog here:          |`)
